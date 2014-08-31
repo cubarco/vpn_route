d1852d1b78ff4b8cc348383159a4d6c92d5f7409
CubeLin <marco.l.here@gmail.com>
2014-08-31 18:01:11 +0800

    exclude ipv6 routes when grep

--- vpn_route.sh
+++ vpn_route.sh
@@ -7,7 +7,7 @@ VPN_SERVER_IP="ip.of.your.vpn"
 # Unneccessary to change
 VPN_IF_NAME="ppp0"
 
-DEFAULT_ROUTE=$( ip route show table all | grep -e '^default' | grep -v $VPN_IF_NAME )
+DEFAULT_ROUTE=$( ip route show table all | grep -e '^default' | grep -v $VPN_IF_NAME | grep -v 'fe80')
 DEFAULT_TABLE_ID=$( echo $DEFAULT_ROUTE | sed -n 's/.*table\s*\([0-9]*\)/\1/p' )
 DEFAULT_DEV=$( echo $DEFAULT_ROUTE | sed -n 's/.*dev\s*\([0-9a-zA-Z]*\).*/\1/p' )
 DEFAULT_GW=$( echo $DEFAULT_ROUTE | sed -n 's/.*\s\(\([0-9]\{1,3\}\.\)\{3\}[0-9]\{1,3\}\).*/\1/p' )
